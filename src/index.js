797e1c6fe8e99677e12f833b9ae6b8db767ac92d
Kanstantsin Kulak <kkulak24@gmail.com>
2020-03-03 00:44:06 +0300

    feat: implement checking brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var brackets = new Object();
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const it = bracketsConfig[i];
+    brackets[it[0]] = it[1];
+  }
+  var stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const el = str[i];
+    // if stack is empty
+    if (stack.length == 0) {
+      // if it is not left bracket
+      if (brackets[el] == undefined) {
+        return false;
+      } else {
+        stack.push(el);
+      }
+    } else {
+      // if it is not left bracket
+      if (brackets[el] == undefined) {
+        const last = stack[stack.length - 1];
+        // check is it suitable right bracket
+        if (el == brackets[last]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      } else {
+        if (brackets[el] != el) {
+          stack.push(el);
+        } else {
+          const last = stack[stack.length - 1];
+          // check is it suitable right bracket
+          if (el == brackets[last]) {
+            stack.pop();
+          } else {
+            stack.push(el);
+          }
+        }
+      }
+    }
+  }
+  if (stack.length == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
